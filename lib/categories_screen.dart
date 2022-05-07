22c1be00638fe23f5f1eb9ea409e567121143692
ASUS <eb.mehrdad@gmail.com>
2022-05-07 10:12:39 +0430

    cat screen and item created

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
-
+import './category_item.dart';
+import 'dummy_data.dart';
 class name extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: <Widget>[],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+      children: DUMMY_CATEGORIES.map((catgory) => CategoryItem(catgory.title, catgory.color)).toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent( // SliverGridDelegateWithMaxCrossAxisExtent is a widget that allows you to set the max cross axis extent of the grid.
           maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
+          childAspectRatio: 3 / 2, 
           crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
+          mainAxisSpacing: 20
+          ),
     );
   }
 }
