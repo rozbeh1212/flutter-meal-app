7da63e819e5d6a79ef0af2cfa51ce36d7ba5b119
ASUS <eb.mehrdad@gmail.com>
2022-05-08 13:23:21 +0430

    sub folder created

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: unused_local_variable
 
 import 'package:flutter/material.dart';
-
+import './dummy_data.dart';
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
   // final String categoryId;
@@ -10,10 +10,13 @@ class CategoryMealsScreen extends StatelessWidget {
   CategoryMealsScreen();
   @override
   Widget build(BuildContext context) {
-    final routeArgs = //
-        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final routeArgs =  ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryId = routeArgs['id'];
     final categoryTitle = routeArgs['title'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
@@ -25,7 +28,7 @@ class CategoryMealsScreen extends StatelessWidget {
               padding: const EdgeInsets.all(20),
             ),
           );
-        },
+        }, itemCount: categoryMeals.length,
       ),
     );
   }
--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'category_meals_screen.dart';
+import '../screens/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String id;
   final String title;
   final Color color;
 
-  const CategoryItem(this.id ,this.title, this.color);
+  const CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext ctx) {
     Navigator.of(ctx).pushNamed(
@@ -17,6 +17,7 @@ class CategoryItem extends StatelessWidget {
         'title': title,
       },
     );
+  }
 
   @override
   Widget build(BuildContext context) {
