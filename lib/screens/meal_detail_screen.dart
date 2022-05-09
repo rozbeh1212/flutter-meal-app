0328f8960550b85996a64962eab85be455eee6b3
ASUS <eb.mehrdad@gmail.com>
2022-05-09 13:57:12 +0430

    tap to show detail modified

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
 class MealDetailScreen extends StatelessWidget {
+  static const routeName = '/meal-detail';
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Meal Detail Screen'),
+    final mealId = ModalRoute.of(context).settings.arguments
+        as String; //mealId is the argument passed from the previous screen
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meal Detail'),),
+      body: Center(
+        child: Text('Meal  $mealId'),
+      ),
+      
     );
   }
 }
