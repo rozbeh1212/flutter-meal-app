bd32c741fed86755d05554512bc394498f51e99d
ASUS <eb.mehrdad@gmail.com>
2022-05-10 13:26:36 +0430

    tab screen logic modified

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
 
-class TabsScreenState extends StatelessWidget {
+import 'categories_screen.dart';
+import 'favorites_screen.dart';
+
+class TabsScreen extends StatefulWidget {
   @override
   _TabsScreenState createState() => _TabsScreenState();
 }
 
-  class _TabsScreenState extends State<TabsScreenState> {
+class _TabsScreenState extends State<TabsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Meals'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.category), text: 'Categories'),
+              Tab(icon: Icon(Icons.star), text: 'Favorites'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            CategoriesScreen(),
+            FavoritesScreen(),
+          ],
+        ),
+      ),
+    );
   }
 }
